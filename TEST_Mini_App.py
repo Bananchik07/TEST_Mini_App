d218c25d418443908c8e5c09d3df3c734ebe6b97
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 20:40:28 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -11,12 +11,10 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #st.set_page_config(page_title='Green Coffee in the USA', page_icon='☕️', layout='wide', initial_sidebar_state='expanded')
 
-pages = [st.Page('pages/page_account.py',title='Account♿'), 
+pages = [st.Page('pages/page_account.py',title='Account♿', default=True), 
          st.Page('pages/page_my_courses.py',title='My courses📝'), 
          st.Page('pages/page_find_courses.py',title='Find course🔍'), 
          st.Page('pages/page_settings.py',title='Settings⚙️')]
-
-# !!!!!  pages/fresh_dashboard_page.py !!!!!
 
 pg = st.navigation(pages,position='sidebar')
 pg.run()
