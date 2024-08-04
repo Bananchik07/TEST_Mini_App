c0015fe15c4af345c81df6f4a73d7459dd1ef83d
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 18:10:47 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -5,6 +5,9 @@ st.title("ТЕСТ")
 VIDEO_URL = "https://vss6.coursehunter.net/s/c7119c234f428763529be8af167d21ad/udemy-megapython/lesson1.mp4"
 st.video(VIDEO_URL)
 
+st.sidebar.title("Бічна панель")
+st.sidebar.selectbox("Виберіть опцію", ["Опція 1", "Опція 2"])
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
