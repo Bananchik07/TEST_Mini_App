99111ed7a0814f87a32287ce2ee12ebf1b8da29b
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 22:14:58 +0300

    Update page_account.py

--- pages/page_account.py
+++ pages/page_account.py
@@ -11,6 +11,18 @@ def gif():
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+# Додавання стилів для зменшення верхнього відступу
+st.markdown(
+    """
+    <style>
+    .main .block-container {
+        padding-top: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title('Account♿')
 
 gif()
