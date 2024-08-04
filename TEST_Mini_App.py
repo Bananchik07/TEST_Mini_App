8f475f8e9d4e29a239f2ab1e3d8db52cc6d7f9b0
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 13:14:40 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -1,4 +1,7 @@
 import streamlit as st
+
+st.title("ТЕСТ")
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -6,6 +9,3 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.title("ТЕСТ!")
-
-
