05f97c78548587219894066ee2a429e8ff020e6f
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 18:43:09 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -1,20 +1,41 @@
 import streamlit as st
 
-st.title("ТЕСТ")
+# CSS для прозорого фону та контуру кнопок
+custom_css = """
+    <style>
+    .stButton > button {
+        background-color: transparent;
+        border: 2px solid transparent;
+        color: white;
+        font-size: 16px;
+        font-weight: bold;
+    }
+    .stButton > button:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+        border: 2px solid #000000;
+    }
+    .stButton > button:active {
+    color: white;
+    background-color: grey;
+    }
+    </style>
+"""
 
-VIDEO_URL = "https://vss6.coursehunter.net/s/c7119c234f428763529be8af167d21ad/udemy-megapython/lesson1.mp4"
-st.video(VIDEO_URL)
+# Додавання стилів
+st.markdown(custom_css, unsafe_allow_html=True)
 
-st.sidebar.title("Бічна панель")
-st.sidebar.selectbox("Виберіть опцію", ["Опція 1", "Опція 2"])
+st.sidebar.title("Main menu")
+st.sidebar.button("Account♿", type="primary")
+st.sidebar.button("My courses📝", type="primary")
+st.sidebar.button("Find course🔍", type="primary")
+st.sidebar.button("Settings⚙️", type="primary")
+
+
 
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-
