91a4778e15992b0d253ea7e3a6e263c93196e9e5
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 22:15:20 +0300

    Update page_settings.py

--- pages/page_settings.py
+++ pages/page_settings.py
@@ -3,6 +3,18 @@ import streamlit as st
 def gif():
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-tp.gif?v=1523984593", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+# Додавання стилів для зменшення верхнього відступу
+st.markdown(
+    """
+    <style>
+    .main .block-container {
+        padding-top: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title('Settings⚙️')
 
 gif()
