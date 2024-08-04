7c23c513ca95de2b049b6cc70da014267f366243
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 20:40:50 +0300

    Update page_account.py

--- pages/page_account.py
+++ pages/page_account.py
@@ -2,5 +2,13 @@ import streamlit as st
 
 def gif():
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
 gif()
