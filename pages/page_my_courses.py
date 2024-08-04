ed96bf7423f03f666ffe99ee3eabbd647ab620b6
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 22:13:50 +0300

    Update page_my_courses.py

--- pages/page_my_courses.py
+++ pages/page_my_courses.py
@@ -3,4 +3,6 @@ import streamlit as st
 def gif():
     st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHMyYWxrdnMybnZzaWpxYWRtaTIwazJ2eWhhcTljbzZwc2I3cGM2byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gKfNj8cYeGN63bLRkF/giphy.webp", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+st.title('My courses📝')
+
 gif()
