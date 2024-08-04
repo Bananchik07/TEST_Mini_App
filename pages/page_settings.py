0522d9233ac0a8335cc8aded3633490aff01e2b0
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 22:14:17 +0300

    Update page_settings.py

--- pages/page_settings.py
+++ pages/page_settings.py
@@ -3,4 +3,6 @@ import streamlit as st
 def gif():
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-tp.gif?v=1523984593", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+st.title('Settings⚙️')
+
 gif()
