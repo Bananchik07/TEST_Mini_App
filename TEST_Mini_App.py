958b3c1916110a78373dd22e8c36ff24138a3fff
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 15:44:26 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -2,11 +2,15 @@ import streamlit as st
 
 st.title("ТЕСТ")
 
+VIDEO_URL = "https://vss6.coursehunter.net/s/c7119c234f428763529be8af167d21ad/udemy-megapython/lesson1.mp4"
+st.video(VIDEO_URL)
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
