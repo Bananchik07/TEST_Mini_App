103719ec5a4248a67f0a165beae1a26b1424b053
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 13:19:07 +0300

    Update TEST_Mini_App.py

--- TEST_Mini_App.py
+++ TEST_Mini_App.py
@@ -4,8 +4,8 @@ st.title("ТЕСТ")
 
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
