00c4259f8d7ac0cb28a1e576885e69cd0ad056f8
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 20:00:52 +0300

    Update page_find_courses.py

--- pages/page_find_courses.py
+++ pages/page_find_courses.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+def gif():
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-wheel_grande.gif?v=1523982721", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+gif()
