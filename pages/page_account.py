ef9295b3dcb122cf79bd7f4d4fdc76cc8428c25f
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 19:58:48 +0300

    Update page_account.py

--- pages/page_account.py
+++ pages/page_account.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+def gif():
+    st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+gif()
