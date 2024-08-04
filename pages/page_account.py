94c509296505f336f1d497b6cf3833af63d98b41
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-04 22:13:16 +0300

    Update page_account.py

--- pages/page_account.py
+++ pages/page_account.py
@@ -11,4 +11,6 @@ def gif():
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     st.image("https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-maru_grande.gif?v=1523984148", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
+st.title('Account♿')
+
 gif()
