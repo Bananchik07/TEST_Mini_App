c879c684080b3295f7b2051448e76cdae8073bb3
Bananchik07 <131598056+Bananchik07@users.noreply.github.com>
2024-08-05 13:35:52 +0300

    Update page_find_courses.py

--- pages/page_find_courses.py
+++ pages/page_find_courses.py
@@ -14,15 +14,73 @@ st.markdown(
 
 def gif():
     for _ in range(6):
-        st.image(
-            "https://cdn.shopify.com/s/files/1/0344/6469/files/cat-gif-loop-wheel_grande.gif?v=1523982721", 
-            caption=None, 
-            width=None, 
-            use_column_width=None, 
-            clamp=False, 
-            channels="RGB", 
-            output_format="auto"
-        )
+        # HTML та CSS для стилізації кнопки
+        button_html = """
+        <style>
+        .button-container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin-top: 20px;
+        }
+        .course-button {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            border: 2px solid #262730;
+            border-radius: 8px;
+            width: 500px;
+            height: 150px;
+            padding: 10px;
+            background-color: #10131A;
+            text-align: left;
+            cursor: pointer;
+        }
+        .course-button:hover {
+            background-color: #141921;
+        }
+        .course-image {
+            width: 120px;
+            height: 120px;
+            margin-right: 10px;
+        }
+        .course-content {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            width: 100%;
+        }
+        .course-title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+        .course-description {
+            flex-grow: 1;
+            font-size: 16px;
+            margin-bottom: 10px;
+        }
+        .course-price {
+            text-align: right;
+            font-size: 20px;
+            font-weight: bold;
+        }
+    </style>
+
+    <div class="button-container">
+        <div class="course-button" onclick="window.location.href='#'">
+            <img src="https://via.placeholder.com/120" class="course-image" alt="Course Image">
+            <div class="course-content">
+                <div class="course-title">Course name</div>
+                <div class="course-description">description</div>
+                <div class="course-price">$Prise$</div>
+            </div>
+        </div>
+    </div>
+    """
+
+        # Відображення кнопки у Streamlit
+        st.markdown(button_html, unsafe_allow_html=True)
 
 st.title('Find course🔍')
 
@@ -33,6 +91,6 @@ with col1:
     option1 = st.selectbox('Choose category:', ['XXX', 'Anal', 'Gay'])
 
 with col2:
-    option2 = st.selectbox('Choose lang🌎:', ['Eng🇺🇸', 'Rus💩'])
+    option2 = st.selectbox('Choose lang🌎:', ['Eng', 'Rus'])
 
 gif()
